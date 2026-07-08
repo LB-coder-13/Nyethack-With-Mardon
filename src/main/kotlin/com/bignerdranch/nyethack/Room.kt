20abfc10cd3bd1a5a16c94088d527e347c1220dc
MardonCoder <Mardonfayzullaev2006@gmail.com>
2026-07-08 15:47:05 +0300

    second commit

---
+++ src/main/kotlin/com/bignerdranch/nyethack/Room.kt
@@ -0,0 +1,63 @@
+package com.bignerdranch.nyethack
+
+open class Room(val name: String) {
+
+    protected open val status = "Calm"
+
+
+    open fun description() = name
+
+    open fun enterRoom() {
+        narrate("There is nothing to do here")
+    }
+}
+
+open class TownSquare : Room("The town square"){
+    private var bellSound = "GWONG"
+    override val status = "Bustling"
+
+    override fun enterRoom(){
+        narrate("The villagers rally and cheer as the hero enters")
+    }
+    fun ringBell() {
+        narrate("The bell tower announces the hero's presence: $bellSound")
+    }
+}
+
+
+open class MonsterRoom(
+    name: String,
+    var monster: Monster? = Goblin()
+) : Room(name) {
+    override fun description() =
+        super.description() + " (Creature: ${monster?.description ?: "None"})"
+    override fun enterRoom() {
+        if (monster == null) {
+            super.enterRoom()
+        } else {
+            narrate("Danger is lurking in this room")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
